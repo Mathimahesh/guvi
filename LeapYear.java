28520b839be9634f26002d46aa230d0af6f4d88e
Mathimahesh <36835393+Mathimahesh@users.noreply.github.com>
2018-09-02 10:43:09 +0530

    Create  LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the year");
+		int n=sc.nextInt();
+		if(n%2==0) {
+			System.out.println("Yes");
+		}
+		else {
+			System.out.println("No");
+		}
+	}
+
+}
