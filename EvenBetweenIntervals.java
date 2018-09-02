d7c087caa971ff7b74cdefd2d225b80b6f4b1a08
Mathimahesh <36835393+Mathimahesh@users.noreply.github.com>
2018-09-02 11:34:29 +0530

    Create EvenBetweenIntervals.java

---
+++ EvenBetweenIntervals.java
@@ -0,0 +1,15 @@
+public class EvenBetweenIntervals {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two intervals ");
+		int m=sc.nextInt();
+		int n=sc.nextInt();
+		for(int i=m;i<n;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
