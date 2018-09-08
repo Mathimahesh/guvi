19b9a348487112d5f210b5b4dad31153604c3322
Mathimahesh <36835393+Mathimahesh@users.noreply.github.com>
2018-09-08 10:43:12 +0530

    Update and rename Factorial.java  to Factorial1.java

---
+++ Factorial1.java
@@ -0,0 +1,15 @@
+public class Factorial1 {
+
+	public static void main(String[] args) {
+		int fact=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			fact=fact*i;
+		}
+		System.out.println(fact);
+		
+	}
+
+}
