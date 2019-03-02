a8282c255c70ddad3cfa2b37e2d26d007307e131
Mathimahesh <36835393+Mathimahesh@users.noreply.github.com>
2019-03-02 18:40:23 +0530

    Update firstAppear.java

--- firstAppear.java
+++ firstAppear.java
@@ -10,9 +10,7 @@ public class MyClass {
         int index = -1;
         a:
         for(int i=0;i<a.length-1;i++){
-            System.out.print("i"+i+" ");
             for(int j=i+1;j<a.length;j++){
-                System.out.print("j"+j+" ");
                 if(a[i]==a[j]){
                     index = a[i];
                     break a;
