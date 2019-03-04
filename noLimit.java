7b58b8d5ac2af990a38bfd2649dff2169fd52c25
Mathimahesh <36835393+Mathimahesh@users.noreply.github.com>
2019-03-04 20:03:40 +0530

    Update noLimit.java

--- noLimit.java
+++ noLimit.java
@@ -4,7 +4,7 @@ public class MyClass {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if(n<=1 && n>=9) System.out.println("yes");
+        if(n>=1 && n<=10) System.out.println("yes");
         else System.out.println("no");
     }
 }
