74d9f9b346dac21783639e551c27f18ed6b75687
Mathimahesh <36835393+Mathimahesh@users.noreply.github.com>
2019-03-04 20:02:30 +0530

    Create noLimit.java

---
+++ noLimit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.time.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<=1 && n>=9) System.out.println("yes");
+        else System.out.println("no");
+    }
+}
